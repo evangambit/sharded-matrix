8a2420423888535119ac0cda0136fdf1f2a2d7cf
Morgan Redding <evangambit@gmail.com>
2024-07-13 18:09:02 +0800

    Add threading support

--- sharded_matrix.h
+++ sharded_matrix.h
@@ -3,9 +3,9 @@
 #include <cstdint>
 #include <vector>
 
-/**
- * TODO: take advantage of sharded matrices to support multi-threading.
- */
+namespace ShardedMatrix {
+
+namespace {
 
 std::string path2shardname(const std::string& path, int i) {
   std::string n = std::to_string(i);
@@ -20,96 +20,78 @@ uint32_t product(const std::vector<uint32_t>& A) {
   return p;
 }
 
+/** Targeted shard size (25 MB). */
 const uint32_t kShardSizeBytes = 25'000'000;
 
+/**
+ * An internal class with a 1-to-1 relationship with a shard file.
+ */
 template<class T>
-class ShardedWriter {
+class ShardWriter {
 public:
-  const std::string filename;
-  const std::vector<uint32_t> dims;
-  const size_t bytesPerRow;
-  const size_t rowsPerShard;
-  uint32_t shardCounter;
-  uint32_t rowCounter;
-  std::ofstream file;
-  ShardedWriter(const std::string& filename, const std::vector<uint32_t>& dims)
-  : filename(filename), dims(dims),
-    shardCounter(-1), rowCounter(0),
-    bytesPerRow(product(dims) * sizeof(T)),
-    rowsPerShard(kShardSizeBytes / (product(dims) * sizeof(T))) {
+  ShardWriter(const std::string shardpath, const std::vector<uint32_t>& dims)
+  : path_(shardpath), dims_(dims), rowCounter_(0),
+  bytesPerRow(product(dims) * sizeof(T)),
+  rowsPerShard(kShardSizeBytes / (product(dims) * sizeof(T))) {
     if (this->rowsPerShard == 0) {
       throw std::runtime_error("Shard size is too small");
     }
     this->_init_file();
   }
 
-  static std::string type_code();
-  
-  void write_row(const T *const row) {
-    this->_write_row(row);
-    if (++rowCounter >= this->rowsPerShard) {
-      this->_close();
-      rowCounter = 0;
-      this->_init_file();
-    }
-  }
-
-  void _init_file() {
-    std::string path = path2shardname(filename, ++shardCounter);
-    file.open(path, std::ios::binary | std::ios::out);
-    const std::string typeCode = ShardedWriter<T>::type_code();
-    const uint32_t numDims = dims.size() + 1;
-    file.write((char *)(typeCode.data()), 4);
-    file.write((char *)(&numDims), sizeof(uint32_t));
-    file.write((char *)(&rowCounter), sizeof(uint32_t));
-    file.write((char *)(dims.data()), dims.size() * sizeof(uint32_t));
-  }
-
-  ~ShardedWriter() {
+  ShardWriter(const ShardWriter&) = delete;
+  ShardWriter& operator=(const ShardWriter&) = delete;
+  ~ShardWriter() {
     this->_close();
   }
 
-  void _close() {
-    file.seekp(8);
-    file.write((const char*)(&rowCounter), sizeof(uint32_t));
-    file.close();
-    rowCounter = 0;
+  /**
+   * Write a row to the shard.
+   * 
+   * @param row The row to write.
+   * @return Is the shard is full?
+   */
+  bool write_row(const T *const row) {
+    this->_write_row(row);
+    return ++rowCounter_ >= this->rowsPerShard;
+  }
+
+private:
+  void _init_file() {
+    file_.open(path_, std::ios::binary | std::ios::out);
+    const std::string typeCode = ShardWriter<T>::_type_code();
+    const uint32_t numDims = dims_.size() + 1;
+    file_.write((char *)(typeCode.data()), 4);
+    file_.write((char *)(&numDims), sizeof(uint32_t));
+    file_.write((char *)(&rowCounter_), sizeof(uint32_t));
+    file_.write((char *)(dims_.data()), dims_.size() * sizeof(uint32_t));
   }
 
   void _write_row(const T *const row) {
-    file.write((char *)(row), this->bytesPerRow);
+    file_.write((char *)(row), this->bytesPerRow);
   }
 
-  void close() {
-    // Update row count
-    this->_close();
+  /** Returns the unique 4-byte string that identifies the type "T" */
+  static std::string _type_code();
+
+  void _close() {
+    file_.seekp(8);
+    file_.write((const char*)(&rowCounter_), sizeof(uint32_t));
+    file_.close();
   }
+
+  const std::string path_;
+  const std::vector<uint32_t> dims_;
+  const size_t bytesPerRow;
+  const size_t rowsPerShard;
+  uint32_t rowCounter_;
+  std::ofstream file_;
 };
 
+/** We override some boolean methods since we'd like to pack 8 bools in a byte. */
 template<>
-std::string ShardedWriter<float>::type_code() {
-  return "f32 ";
-}
-
-template<>
-std::string ShardedWriter<int8_t>::type_code() {
-  return "i8  ";
-}
-
-template<>
-std::string ShardedWriter<int16_t>::type_code() {
-  return "i16  ";
-}
-
-template<>
-std::string ShardedWriter<bool>::type_code() {
-  return "bool";
-}
-
-template<>
-ShardedWriter<bool>::ShardedWriter(const std::string& filename, const std::vector<uint32_t>& dims)
-  : filename(filename), dims(dims),
-  shardCounter(-1), rowCounter(0),
+ShardWriter<bool>::ShardWriter(const std::string path, const std::vector<uint32_t>& dims)
+  : path_(path), dims_(dims), rowCounter_(0),
   bytesPerRow(product(dims) / 8),
   rowsPerShard(kShardSizeBytes / (product(dims) / 8)) {
   if (this->rowsPerShard == 0) {
@@ -122,12 +104,110 @@ ShardedWriter<bool>::ShardedWriter(const std::string& filename, const std::vecto
 }
 
 template<>
-void ShardedWriter<bool>::_write_row(const bool *const row) {
+void ShardWriter<bool>::_write_row(const bool *const row) {
   // Write to file
   int8_t *A = new int8_t[this->bytesPerRow];
   std::fill_n(A, this->bytesPerRow, 0);
   for (int i = 0; i < this->bytesPerRow; ++i) {
     A[i] = row[8 * i + 0] << 0 | row[8 * i + 1] << 1 | row[8 * i + 2] << 2 | row[8 * i + 3] << 3 | row[8 * i + 4] << 4 | row[8 * i + 5] << 5 | row[8 * i + 6] << 6 | row[8 * i + 7] << 7;
   }
-  file.write((const char*)A, this->bytesPerRow);
+  file_.write((const char*)A, this->bytesPerRow);
 }
+
+
+template<>
+std::string ShardWriter<float>::_type_code() {
+  return "f32 ";
+}
+
+template<>
+std::string ShardWriter<int8_t>::_type_code() {
+  return "i8  ";
+}
+
+template<>
+std::string ShardWriter<int16_t>::_type_code() {
+  return "i16  ";
+}
+
+template<>
+std::string ShardWriter<bool>::_type_code() {
+  return "bool";
+}
+
+template<class T>
+class WriterDelegate {
+public:
+  WriterDelegate(const std::string& filename) : filename_(filename), shardCounter_(-1) {}
+
+  std::string new_shard_path() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    std::string result = path2shardname(filename_, ++shardCounter_);
+    return result;
+  }
+private:
+  std::string filename_;
+  uint32_t shardCounter_;
+  std::mutex mutex_;
+};
+
+}  // namespace
+
+template<class T>
+class WriterManager;
+
+/**
+ * An external class that manages the writing to a sharded matrix.
+ * 
+ * When one shard is filled, Writer requests a new shard automatically.
+ */
+template<class T>
+class Writer {
+public:
+  friend WriterManager<T>;
+
+  /** Public constructor that can be used in single-threaded contexts. */
+  Writer(const std::string filename, const std::vector<uint32_t>& dims)
+  : dims_(dims) {
+    delegate_ = new WriterDelegate<T>(filename);
+    internalWriter_ = std::make_unique<ShardWriter<T>>(delegate_->new_shard_path(), dims);
+  }
+
+  void write_row(const T *const row) {
+    if (this->internalWriter_->write_row(row)) {
+      // Create new writer
+      this->internalWriter_.reset(new ShardWriter<T>(delegate_->new_shard_path(), dims_));
+    }
+  }
+
+private:
+  /** Private constructor used by WriterManager */
+  Writer(const std::vector<uint32_t>& dims, WriterDelegate<T> *delegate)
+  : dims_(dims), internalWriter_(new ShardWriter<T>(delegate->new_shard_path(), dims)), delegate_(delegate) {}
+
+  std::vector<uint32_t> dims_;
+  std::unique_ptr<ShardWriter<T>> internalWriter_;
+  WriterDelegate<T> *delegate_;
+};
+
+/**
+ * Manages multiple Writers for a single sharded matrix. Useful when there are
+ * multiple threads.
+ */
+template<class T>
+class WriterManager {
+public:
+  WriterManager(const std::string& filename, const std::vector<uint32_t>& dims)
+  : writerDelegate_(filename), dims_(dims), shardCounter_(-1) {}
+
+  /** Each thread should call this to get its own writer. */
+  std::shared_ptr<Writer<T>> new_shard_writer() {
+    return std::shared_ptr<Writer<T>>(new Writer<T>(dims, &(this->writerDelegate_)));
+  }
+private:
+  const std::vector<uint32_t> dims_;
+  uint32_t shardCounter_;
+  WriterDelegate<T> writerDelegate_;
+};
+
+}  // namespace ShardedMatrix
